ca3656ff56ed315af773313dfb96a4e4202f172a
uvsq22008847 <shuyang.zhou@ens.uvsq.fr>
2021-09-30 14:27:03 +0200

    Add tostring

--- TD1/src/TD1/Fraction.java
+++ TD1/src/TD1/Fraction.java
@@ -1,10 +1,22 @@
 package TD1;
 
 public class Fraction {
+	int numerateur;
+	int denominateur;
+	
+	Fraction(int num, int denom){
+		this.numerateur = num;
+		this.denominateur = denom;
+	}
+	
+	public String toString() {
+		return "numerateur:" + this.numerateur + "\n" + "denominateur" + this.denominateur;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Helloworld");
+		Fraction f = new Fraction(2,5);
+		System.out.println(f.toString());
 	}
 
 }
